b32851d0e5ae7d60ecde856e9597dd680658d1f6
balistik <balistiktw@gmail.com>
2023-11-18 22:47:11 +1000

    make relationships with User and Request

--- bot/db/model.py
+++ bot/db/model.py
@@ -1,6 +1,8 @@
-from sqlalchemy import BigInteger, String
+from sqlalchemy import BigInteger, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+from typing import List
 
 from config import DATABASE_URL
 
@@ -12,13 +14,28 @@ class Base(AsyncAttrs, DeclarativeBase):
   pass
 
 class User(Base):
-  __tablename__ = 'user'
+  __tablename__ = 'user_table'
 
   id: Mapped[int] = mapped_column(primary_key=True)
-  tg_id = mapped_column(BigInteger)
-  fio = mapped_column(String)
-  phone_numebr = mapped_column(String)
-  adress = mapped_column(String)
+  tg_id: Mapped[int] = mapped_column(BigInteger)
+  fio: Mapped[str] = mapped_column(String)
+  phone_numebr: Mapped[str] = mapped_column(String)
+  requests: Mapped[List['Request']] = relationship(back_populates="user")
+
+class Request(Base):
+  __tablename__ = 'request_table'
+
+  id: Mapped[int] = mapped_column(primary_key=True)
+  description: Mapped[str] = mapped_column(String)
+  address: Mapped[str] = mapped_column(String)
+  category: Mapped[str] = mapped_column(String)
+  date: Mapped[str] = mapped_column(String)
+  time: Mapped[str] = mapped_column(String)
+  cost_min: Mapped[int] = mapped_column(Integer)
+  cost_max: Mapped[int] = mapped_column(Integer)
+  user_id: Mapped[int] = mapped_column(ForeignKey('user_table.id'))
+  user: Mapped["User"] = relationship(back_populates='requests')
+
 
 async def async_main():
   async with engine.begin() as conn:
