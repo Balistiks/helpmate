547756fc2e72a8ee63032d5a725ae9f8e4f86853
balistik <balistiktw@gmail.com>
2023-11-19 16:36:19 +1000

    change style

--- web-app/src/pages/add_request/index.jsx
+++ web-app/src/pages/add_request/index.jsx
@@ -6,29 +6,45 @@ const AddRequestPage = () => {
   const [description, setDescription] = useState('');
   const [adress, setAdress] = useState('');
   const [category, setCategory] = useState('');
-  const [data, setData] = useState('');
+  const [date, setDate] = useState('');
   const [time, setTime] = useState('');
 
   return (
-    <>
-      <p>Напишите описание услуги:</p>
-      <Input value={description} onChange={(value) => setDescription(value)} />
-      <p>Напишите описание услуги:</p>
-      <Input value={adress} onChange={(value) => setAdress(value)} />
-      <p>Выберите категорию</p>
-      <div>
-        <select value={category} onChange={(event) => setCategory(event.target.value)}>
-          <option value='сантехника'>сантехника</option>
-          <option value='репетитор'>репетитор</option>
-          <option value='инструктор по вождению'>инструктор по вождению</option>
-          <option value='клининг'>клининг</option>
+    <div className="add-request-container">
+      <h2>Добавить Запрос</h2>
+      <div className="form-group">
+        <label htmlFor="description">Напишите описание услуги:</label>
+        <Input value={description} onChange={(value) => setDescription(value)} />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="address">Напишите адрес:</label>
+        <Input value={adress} onChange={(value) => setAdress(value)} />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="category">Выберите категорию:</label>
+        <select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="сантехника">Сантехника</option>
+          <option value="репетитор">Репетитор</option>
+          <option value="инструктор по вождению">Инструктор по вождению</option>
+          <option value="клининг">Клининг</option>
         </select>
       </div>
-      <p>Напишите желаемую дату:</p>
-      <Input value={data} onChange={(value) => setData(value)} />
-      <p>Напишите желаемое время:</p>
-      <Input value={time} onChange={(value) => setTime(value)} />
-    </>
+
+      <div className="form-group">
+        <label htmlFor="date">Напишите желаемую дату:</label>
+        <Input value={date} onChange={(value) => setDate(value)} />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="time">Напишите желаемое время:</label>
+        <Input value={time} onChange={(value) => setTime(value)} />
+      </div>
+    </div>
   );
 }
 
